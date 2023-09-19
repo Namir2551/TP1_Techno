098d79371786c358f49ed0fa3bd29fad2f2b766a
Namir2551 <namirkas22@gmail.com>
2023-09-19 10:48:46 -0400

    creation des methode

---
+++ Ex1/ex1.cpp
@@ -0,0 +1,49 @@
+
+#include <iostream>
+
+
+// appele des methodes 
+void trierTab(int*tab, int taille);
+void afficherTab( int *tab, int taille );
+bool egalite( int tab1[ ], int tab2[ ], int taille );
+bool egalitePtr( int *tab1, int *tab2, int taille );
+int * concatenerTab( int *tab1, int *tab2, int taille );
+
+int main(){
+
+
+    const int NB_ELEMENTS = 10;
+    int tableau1[] = {22, 44, 12, 61, 4, 99, 33, 17, 15, 123};
+    int tableau2[] = {22, 33, 44, 61, 4, 99, 12, 17, 123, 15};
+    int tableau3[] = {22, 44, 12, 61, 4, 99, 33, 17, 15, 123};
+
+
+    return 0;
+}
+
+
+// ========== methode ===========
+
+void trierTab(int*tab, int taille){
+
+}
+
+
+void afficherTab( int *tab, int taille ){
+
+}
+
+bool egalite( int tab1[ ], int tab2[ ], int taille ){
+
+}
+
+bool egalitePtr( int *tab1, int *tab2, int taille ){
+
+}
+
+int * concatenerTab( int *tab1, int *tab2, int taille ){
+
+}
+
+
+
